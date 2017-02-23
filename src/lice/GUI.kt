9365794d6c8177ecf6247c931b02e8dd35b32db7
ice1000 <ice1000kotlin@foxmail.com>
2017-02-23 22:02:03 +0800

    move

---
+++ src/lice/GUI.kt
@@ -0,0 +1,64 @@
+/**
+ * Created by ice1000 on 2017/2/23.
+ *
+ * @author ice1000
+ */
+package lice
+
+import lice.compiler.Repl
+import lice.compiler.VERSION_CODE
+import lice.compiler.util.SymbolList
+import lice.compiler.util.forceRun
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.Font
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+import java.io.OutputStream
+import java.io.PrintStream
+import javax.swing.*
+
+
+fun main(args: Array<String>) {
+	val sl = SymbolList()
+	val frame = JFrame("Lice language interpreter $VERSION_CODE")
+	frame.layout = BorderLayout()
+	val output = JTextArea()
+	output.isEditable = false
+	output.background = Color.LIGHT_GRAY
+	val input = JTextField()
+	output.tabSize = 2
+	forceRun {
+		output.font = Font("Consolas", 0, 12)
+		input.font = Font("Consolas", 0, 16)
+	}
+	val printStream = PrintStream(object : OutputStream() {
+		override fun write(b: Int) =
+				output.append(b.toChar().toString())
+
+		override fun write(b: ByteArray) =
+				output.append(java.lang.String(b).toString())
+	})
+	System.setOut(printStream)
+	System.setErr(printStream)
+	val repl = Repl()
+	input.addKeyListener(object : KeyListener {
+		override fun keyTyped(e: KeyEvent?) = Unit
+		override fun keyReleased(e: KeyEvent?) = Unit
+		override fun keyPressed(e: KeyEvent?) {
+//				println("${e?.keyCode}, ${KeyEvent.VK_ENTER}")
+			if (e != null && e.keyCode == KeyEvent.VK_ENTER) {
+				output.append("${input.text}\n")
+				repl.handle(input.text, sl)
+				input.text = ""
+			}
+		}
+	})
+	frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+	frame.add(JScrollPane(output), BorderLayout.CENTER)
+	frame.add(input, BorderLayout.SOUTH)
+	frame.setLocation(100, 100)
+	frame.setSize(360, 360)
+	frame.isVisible = true
+	input.requestFocus()
+}
--- src/lice/compiler/Main.kt
+++ src/lice/compiler/Main.kt
@@ -2,17 +2,8 @@ package lice.compiler
 
 import lice.compiler.parse.createAst
 import lice.compiler.util.SymbolList
-import lice.compiler.util.forceRun
-import java.awt.BorderLayout
-import java.awt.Color
-import java.awt.Font
-import java.awt.event.KeyEvent
-import java.awt.event.KeyListener
 import java.io.File
-import java.io.OutputStream
-import java.io.PrintStream
 import java.util.*
-import javax.swing.*
 
 val VERSION_CODE = "v1.0-SNAPSHOT"
 
@@ -43,48 +34,4 @@ object Main {
 					repl.handle(scanner.nextLine(), sl)
 			} else
 				interpret(File(args[0]))
-}
-
-fun main(args: Array<String>) {
-	val sl = SymbolList()
-	val frame = JFrame("Lice language interpreter $VERSION_CODE")
-	frame.layout = BorderLayout()
-	val output = JTextArea()
-	output.isEditable = false
-	output.background = Color.LIGHT_GRAY
-	val input = JTextField()
-	output.tabSize = 2
-	forceRun {
-		output.font = Font("Consolas", 0, 12)
-		input.font = Font("Consolas", 0, 16)
-	}
-	val printStream = PrintStream(object : OutputStream() {
-		override fun write(b: Int) =
-				output.append(b.toChar().toString())
-
-		override fun write(b: ByteArray) =
-				output.append(java.lang.String(b).toString())
-	})
-	System.setOut(printStream)
-	System.setErr(printStream)
-	val repl = Repl()
-	input.addKeyListener(object : KeyListener {
-		override fun keyTyped(e: KeyEvent?) = Unit
-		override fun keyReleased(e: KeyEvent?) = Unit
-		override fun keyPressed(e: KeyEvent?) {
-//				println("${e?.keyCode}, ${KeyEvent.VK_ENTER}")
-			if (e != null && e.keyCode == KeyEvent.VK_ENTER) {
-				output.append("${input.text}\n")
-				repl.handle(input.text, sl)
-				input.text = ""
-			}
-		}
-	})
-	frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-	frame.add(JScrollPane(output), BorderLayout.CENTER)
-	frame.add(input, BorderLayout.SOUTH)
-	frame.setLocation(100, 100)
-	frame.setSize(360, 360)
-	frame.isVisible = true
-	input.requestFocus()
 }
