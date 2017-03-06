25f5be5beec30257b9f0105416e4217744f5f3af
ice1000 <ice1000kotlin@foxmail.com>
2017-03-06 11:30:05 +0800

    Finished Android apk

--- android/src/main/java/lice/android/MainActivity.kt
+++ android/src/main/java/lice/android/MainActivity.kt
@@ -2,11 +2,37 @@ package lice.android
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import lice.repl.Repl
+import lice.repl.Repl.Companion.HINT
+import lice.repl.VERSION_CODE
+import java.io.OutputStream
+import java.io.PrintStream
 
 class MainActivity : Activity() {
 
 	override fun onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		val out = PrintStream(object : OutputStream() {
+			override fun write(b: Int) =
+					code_output.append(b.toChar().toString())
+
+			override fun write(b: ByteArray) =
+					code_output.append(String(b))
+		})
+		System.setOut(out)
+		System.setErr(out)
+		code_output.append("This is the Android repl $VERSION_CODE for Lice language.\n")
+		val repl = Repl()
+		clear_screen.setOnClickListener {
+			code_output.text = HINT
+		}
+		submit.setOnClickListener {
+			val txt = code_input.text.toString()
+			code_input.text.clear()
+			code_output.append(txt)
+			repl.handle(txt)
+		}
 	}
 }
