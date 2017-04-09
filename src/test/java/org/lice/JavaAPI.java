66a8f9a1f32c8ffa7e92439271556241380e96d8
ice1000 <ice1000kotlin@foxmail.com>
2017-04-09 23:59:21 +0800

    move to java6

--- src/test/java/org/lice/JavaAPI.java
+++ src/test/java/org/lice/JavaAPI.java
@@ -1,11 +1,16 @@
 package org.lice;
 
 import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+import kotlin.jvm.functions.Function2;
+import org.lice.compiler.model.MetaData;
+import org.lice.compiler.model.Node;
 import org.lice.compiler.model.ValueNode;
 import org.lice.compiler.util.SymbolList;
 import org.lice.compiler.util.Utilities;
 
 import java.io.File;
+import java.util.List;
 
 import static org.lice.compiler.parse.Parse.createRootNode;
 
@@ -16,12 +21,20 @@ import static org.lice.compiler.parse.Parse.createRootNode;
  */
 public class JavaAPI {
 	public static void main(String[] args) {
-		SymbolList sl = new SymbolList();
+		final SymbolList sl = new SymbolList();
 		final int[] a = {0};
-		sl.defineFunction("java-api-invoking", (ln, ls) -> new ValueNode(a[0]++, ln));
-		Utilities.forceRun(() -> {
-			createRootNode(new File("sample/test10.lice"), sl).eval();
-			return Unit.INSTANCE;
+		sl.defineFunction("java-api-invoking", new Function2<MetaData, List<? extends Node>, Node>() {
+			@Override
+			public Node invoke(MetaData ln, List<? extends Node> ls) {
+				return new ValueNode(a[0]++, ln);
+			}
+		});
+		Utilities.forceRun(new Function0<Unit>() {
+			@Override
+			public Unit invoke() {
+				createRootNode(new File("sample/test10.lice"), sl).eval();
+				return Unit.INSTANCE;
+			}
 		});
 	}
 }
