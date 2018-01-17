7e4681e2263bd0bfe5a6a40d96d559b1c43c7093
ice1000 <ice1000kotlin@foxmail.com>
2018-01-17 11:29:27 +0800

    [ stdlib ] Fix progression

--- src/main/kotlin/org/lice/core/FunctionMangledHolder.kt
+++ src/main/kotlin/org/lice/core/FunctionMangledHolder.kt
@@ -101,7 +101,7 @@ class FunctionMangledHolder(private val symbolList: SymbolList) {
 		}
 	}
 
-	fun `##`(metaData: MetaData, ls: List<Any?>): List<Int> {
+	fun `##`(metaData: MetaData, ls: List<Any?>): Iterable<Int> {
 		if (ls.size < 2) tooFewArgument(2, ls.size, metaData)
 		val a = ls.first()
 		val b = ls[1]
@@ -109,8 +109,8 @@ class FunctionMangledHolder(private val symbolList: SymbolList) {
 			a is Number && b is Number -> {
 				val begin = a.toInt()
 				val end = b.toInt()
-				if (begin <= end) (begin..end).toList()
-				else (end..begin).reversed().toList()
+				if (begin <= end) (begin..end)
+				else (end..begin).reversed()
 			}
 			else -> typeMisMatch("Number", a as? Number ?: b, metaData)
 		}
@@ -118,8 +118,5 @@ class FunctionMangledHolder(private val symbolList: SymbolList) {
 
 	fun `list-$array`(meta: MetaData, it: List<Any?>) = cast<List<*>>(it.first(meta)).toTypedArray()
 	fun `array-$list`(meta: MetaData, it: List<Any?>) = cast<Array<*>>(it.first(meta)).toList()
-	fun `-$chars`(meta: MetaData, it: List<Any?>) = it.fold(StringBuilder(it.size)) { sb, value ->
-		sb.append(value.toString())
-	}.toString().toCharArray().toList()
-
+	fun `-$chars`(meta: MetaData, it: List<Any?>) = it.joinToString("").toCharArray()
 }
